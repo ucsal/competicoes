a243c3a7c4aed37202b74f5ba944013702dfa2d1
Mario Jorge Pereira <mariojp@gmail.com>
2017-06-11 19:33:09 -0300

    Aquecimento 2016 COMPLETO

---
+++ 2016/primeirafase/aquecimento/B/src/main/java/B.java
@@ -0,0 +1,8 @@
+
+public class B {
+
+	public static void main(String[] args) {
+		//FIXME Apague essa linha e escreva seu codigo
+	}
+
+}
---
+++ 2016/primeirafase/aquecimento/C/src/main/java/C.java
@@ -0,0 +1,8 @@
+
+public class C {
+
+	public static void main(String[] args) {
+		//FIXME Apague essa linha e escreva seu codigo
+	}
+
+}
