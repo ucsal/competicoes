90ddbf714f990d53bbcc2be3f69cf8cd4717ea20
Mario Jorge Pereira <mariojp@gmail.com>
2017-06-11 22:01:37 -0300

    Solução Comentada do Aquecimento A

--- 2016/primeirafase/aquecimento/A/src/main/java/A.java
+++ 2016/primeirafase/aquecimento/A/src/main/java/A.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class A {
 
 	public static void main(String[] args) {
-		//FIXME Apague essa linha e escreva seu codigo
+		Scanner scanner = new Scanner(System.in);
+		long r = scanner.nextInt(16);
+		long g = scanner.nextInt(16);
+		long b = scanner.nextInt(16);
+		scanner.close();
+		long red = 1; //red
+		long green = (int) (r / g);
+		green = green * green; // green
+		long blue = g / b;
+		blue = blue * blue;
+		blue = blue * green;
+		long sum = red + green + blue;
+		System.out.println(Long.toHexString(sum));
 	}
 
 }
