5841cc81b1107f5e663a1a53f9dd764f0f43da62
Mario Jorge Pereira <mariojp@gmail.com>
2017-06-25 11:11:19 -0300

    Solução Problema C do Aquecimento

--- 2016/primeirafase/aquecimento/C/src/main/java/C.java
+++ 2016/primeirafase/aquecimento/C/src/main/java/C.java
@@ -1,8 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class C {
 
 	public static void main(String[] args) {
-		//FIXME Apague essa linha e escreva seu codigo
+		Scanner scanner = new Scanner(System.in);
+		double h = scanner.nextInt();
+		double p = scanner.nextInt();	
+		scanner.close();
+		double medio = (h / p);
+		java.util.Locale.setDefault(Locale.US);
+		System.out.printf("%.2f\n",medio);
 	}
 
 }
