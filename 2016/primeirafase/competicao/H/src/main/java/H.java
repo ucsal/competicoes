30d58e9a68fca350e4750d2a8f22928615a44d06
Joaldo Tavares <54106303+joaldotavares@users.noreply.github.com>
2020-12-02 23:56:05 -0300

    Update H.java

--- 2016/primeirafase/competicao/H/src/main/java/H.java
+++ 2016/primeirafase/competicao/H/src/main/java/H.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
 
 public class H {
 
 	public static void main(String[] args) {
-		//FIXME Apague essa linha e escreva seu codigo
+		Scanner scan = new Scanner(System.in);
+		String palavra = scan.next().toLowerCase();
+		int j = 0, f = 1;
+		char [] c = palavra.toCharArray();
+		char [] b = new char[50];
+		for (int i = 0; i < c.length; i++) {
+			if(c[i] == 'a' || c[i] == 'e' || c[i] == 'i' || c[i] == 'o' || c[i] == 'u') {
+				b[j] = c[i];
+				j++;
+			}
+		}
+		
+		for (int i = 0; i < j; i++) {
+			if(b[i] != b[j-1-i]) {
+				f = 0;
+				break;
+			}
+		}
+		
+		if(f == 0) {
+			System.out.println("N");
+		}else if(f == 1) {
+			System.out.println("S");
+		}
+		scan.close();
 	}
 
 }
