fd7f29bdb1556d4204d9daed0b16a81c3dac630d
joaldotavares <joaldo.junior@ucsal.edu.br>
2020-12-05 09:13:50 -0300

    Aquecimento e prova 2019. Updade H da prova de 2016

---
+++ 2019/primeirafase/aquecimento/README.md
@@ -0,0 +1,58 @@
+# Maratona de Programação da SBC 2019
+## Sub-Regional Brasil do ACM ICPC
+14 de Setembro de 2019
+
+## Aquecimento
+
+### Limites de tempo
+
+Os tempos são dados em segundos:
+
+| Problema | Nome                 | C/C++ | Java | Kotlin | Python |
+|:--------:|:---------------------|:------------:|:----:|:------:|:------:|
+| A        | Teleferico  		| 1            | 1    | 1      | 1      |
+| B        | Fatorial 			| 1            | 1    | 1      | 1      |
+| C        | Nota Esquecida    | 1            | 1    | 1      | 1      |
+
+
+### Limites de memória
+
+ C, C++, Python: 1GB  
+ Java: 1GB + 100MB pilha
+ Kotlin: 1GB + 100MB pilha  
+
+### Outros limites
+
+ Source file size: 100KB  
+ Output size: 1MB
+
+### Comandos de compilação
+
+C: gcc -static -O2 -lm
+C++: g++ -std=c++14 -static -O2 -lm
+Java: javac
+Kotlin: kotlinc  
+
+### Java
+
+- Não declare package no seu programa java.  
+- Note que a convencão para o nome do arquivo fonte deve ser obedecida, o que significa que o nome de suclasse pública deve ser uma letra maiúscula (A, B ou C).  
+- Comando para executar uma solução java: java -Xms1024m -Xmx1024m -Xss100m
+
+### C/C++
+
+Seu programa deve retornar zero, executando, como último comando, return 0 ou exit(0). 
+ 
+### Kotlin
+
+- Não declare package no seu programa Kotlin.
+- Note que a convenção para o nome do arquivo fonte deve ser obedecida, o que significa que o nome de sua
+classe pública deve ser uma letra maiúscula (A, B ou C).
+- Comando para executar uma solução Kotlin: kotlin -J-Xms1024m -J-Xmx1024m -J-Xss100m
+- Atenção não é garantido que soluções em Kotlin conseguirão executar dentro do tempo limite alocado.
+
+### Python
+
+- Tenha cuidado ao selecionar a versão correta na submissão.
+- Atenção: não é garantido que soluções em Python conseguirão executar dentro do tempo limite alocado.
+
---
+++ 2019/primeirafase/aquecimento/aquecimento_C/src/test/java/Teste.java
@@ -0,0 +1,73 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class Teste {
+	private ByteArrayInputStream inContent;
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+		System.setOut(null);
+		System.setErr(null);
+	}
+
+	private String input;
+	private String expected;
+
+	public Teste(String input, String expected) {
+		this.input = input;
+		this.expected = expected;
+	}
+
+	@Parameters(name = "{index}: main({0})={1}")
+	public static Iterable<Object[]> data() {
+		String inputPath = Teste.class.getClassLoader().getResource("input").getPath();
+		final File inputFolder = new File(inputPath);
+		File[] inputFiles = inputFolder.listFiles();
+		String outputPath = Teste.class.getClassLoader().getResource("output").getPath();
+		final File outputFolder = new File(outputPath);
+		File[] outputFiles = outputFolder.listFiles();
+		Object[][] lista = new Object[inputFiles.length][2];
+
+		for (int i = 0; i < lista.length; i++) {
+			try {
+				lista[i][0] = IOUtils.toString(inputFiles[i].toURI(), "UTF-8");
+				lista[i][1] = IOUtils.toString(outputFiles[i].toURI(), "UTF-8");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return Arrays.asList(lista);
+	}
+
+	@Test(timeout = 1000)
+	public void test() {
+		inContent = new ByteArrayInputStream((input).getBytes());
+		System.setIn(inContent);
+		C.main(null);
+		assertEquals(expected, outContent.toString());
+	}
+
+}
