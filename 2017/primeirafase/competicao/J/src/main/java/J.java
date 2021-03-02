0a395237285d509a835dd180ab91d2e02273248d
Junior Tavares <joaldo.junior@ucsal.edu.br>
2021-03-02 17:34:01 -0300

    Problemas F, J e M da prova de 2017 Resolvidos

--- 2017/primeirafase/competicao/J/src/main/java/J.java
+++ 2017/primeirafase/competicao/J/src/main/java/J.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class J {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String texto;
+		texto = scan.next();
 
+		char vet[] = texto.toCharArray();
+
+		int n = 0;
+
+		for (int i = 0; i < texto.length(); i++) {
+			n += (vet[i] - '0');
+		}
+		System.out.println(n % 3);
+		scan.close();
 	}
-
 }
--- 2017/primeirafase/competicao/M/src/main/java/M.java
+++ 2017/primeirafase/competicao/M/src/main/java/M.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class M {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static Scanner scan;
 
+	public static void main(String[] args) {
+		int a1 = 0, a2 = 0, a3 = 0;
+		List<Integer> lista = new ArrayList<Integer>();
+		scan = new Scanner(System.in);
+
+		a1 = scan.nextInt();
+		a2 = scan.nextInt();
+		a3 = scan.nextInt();
+
+		lista.add((a2 * 2) + (a3 * 4));
+		lista.add((a1 * 2) + (a3 * 2));
+		lista.add((a1 * 4) + (a2 * 2));
+
+		Collections.sort(lista);
+
+		System.out.println(lista.get(0));
 	}
 
 }
