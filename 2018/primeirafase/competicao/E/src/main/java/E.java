dc505617c1a2c523c6c9824ae9627ed727d262f1
Junior Tavares <joaldo.junior@ucsal.edu.br>
2021-03-04 11:31:32 -0300

    Problemas D e E da prova de 2018 resolvidos

--- 2018/primeirafase/competicao/E/src/main/java/E.java
+++ 2018/primeirafase/competicao/E/src/main/java/E.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
 
 public class E {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String chave, crip;
+		crip = scan.next();
+		chave = scan.next();
 
+		char palavra[] = crip.toCharArray();
+		char subPalavra[] = chave.toCharArray();
+
+		int aux = 0;
+
+		for (int i = 0; i < palavra.length - subPalavra.length + 1; i++) {
+			for (int j = 0; j < subPalavra.length; j++) {
+				if (palavra[j + i] == subPalavra[j]) {
+					aux++;
+					break;
+				}
+			}
+		}
+
+		System.out.println((palavra.length - subPalavra.length + 1) - aux);
+		scan.close();
 	}
 
 }
